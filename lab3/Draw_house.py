ff67afe1da296ce0b9aac8d038845de7da0ee87d
Malyshev Vladimir <MalyshevVO@yandex.ru>
2023-04-09 01:04:04 +0300

    НЕ закончил.

--- lab3/Draw_house.py
+++ lab3/Draw_house.py
@@ -1,3 +1,10 @@
+def main():
+    x, y = 300, 400
+    width, heigth = 200, 300
+
+    draw_house(x, y, width, heigth)
+
+
 def draw_house(x, y, width, heigth):
     """
     Нарисовать домик ширины wigth и высоты height от опорной точки (x, y),
@@ -8,11 +15,15 @@ def draw_house(x, y, width, heigth):
     :param heigth: габаритная высота
     :return: None
     """
-
     print('Типа рисую домик...', x, y, width, heigth)
-    pass  # do nothing
+    foundation_height = 0.05 * heigth
+    walls_width = 0.9 * width
+    walls_height = 0.5 * height
+    draw_house_foundation(x, y, width, foundation_height)
+    draw_house_walls(x, y - foundation_height)
+    draw_house_roof()
 
-x, y = 100, 100
-width, heigth = 200, 200
 
-draw_house(x, y, width, heigth)
+
+
+main()
