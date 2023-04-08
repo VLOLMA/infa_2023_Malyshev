afc7c7f5a2f272bba236a6994c6f1aa6737eb8a3
Malyshev Vladimir <MalyshevVO@yandex.ru>
2023-04-08 23:38:05 +0300

    Сделал декомпозицию функции рисования домика.
    Создана функция рисования домика, как заготовка (прототип)
    и докуметирован её интерфейс.
    Если добавить её реализацию, функция будет работать.

---
+++ lab3/Draw_house.py
@@ -0,0 +1,18 @@
+def draw_house(x, y, width, heigth):
+    """
+    Нарисовать домик ширины wigth и высоты height от опорной точки (x, y),
+    которая находится в середине нижней точки фундамента.
+    :param x: координата x середины домика
+    :param y: координата y низа фундамента
+    :param width: габаритная ширина
+    :param heigth: габаритная высота
+    :return: None
+    """
+
+    print('Типа рисую домик...', x, y, width, heigth)
+    pass  # do nothing
+
+x, y = 100, 100
+width, heigth = 200, 200
+
+draw_house(x, y, width, heigth)
